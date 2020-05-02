b45d7556c409b9a6adbd49a253ab5ae519a78c16
beastgetssavvy13 <kavish.pandit178@gmail.com>
2020-05-03 04:40:46 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherform.addEventListener("submit",(e)=>{
     const location=search.value
     messageOne.textContent = "Loading...";
     messageTwo.textContent=""
-    fetch("http://localhost:3000/weather?address="+location).then((response) => {
+    fetch("/weather?address="+location).then((response) => {
       response.json().then((data) => {
         if (data.error) {
           messageOne.textContent=data.error
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const port=process.env.PORT||3000
 const geocode=require("./utils/geocode")
 const forecast = require("./utils/forecast");
 //Define paths for express config
@@ -90,6 +91,6 @@ app.get("*", (req, res) => {
     errorMessage: "Page not found",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up on part 300");
+app.listen(port, () => {
+  console.log("Server is up on part " +port);
 })
